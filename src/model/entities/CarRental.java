e07ec16b08cb3a82bd073ee2186e1b1bde2da81e
Luan Araujo <luanaraujo9977@gmail.com>
2020-12-05 22:43:58 -0300

    implementação sem interface

---
+++ src/model/entities/CarRental.java
@@ -0,0 +1,58 @@
+package model.entities;
+
+import java.util.Date;
+
+public class CarRental {
+
+	private Date start;
+	private Date finsh;
+	
+	private Vehicle vehicle;
+	private Invoice invoice;
+	
+	public CarRental() {
+	}
+
+	public CarRental(Date start, Date finsh, Vehicle vehicle) {
+		super();
+		this.start = start;
+		this.finsh = finsh;
+		this.vehicle = vehicle;
+	}
+
+	public Date getStart() {
+		return start;
+	}
+
+	public void setStart(Date start) {
+		this.start = start;
+	}
+
+	public Date getFinsh() {
+		return finsh;
+	}
+
+	public void setFinsh(Date finsh) {
+		this.finsh = finsh;
+	}
+
+	public Vehicle getVehicle() {
+		return vehicle;
+	}
+
+	public void setVehicle(Vehicle vehicle) {
+		this.vehicle = vehicle;
+	}
+
+	public Invoice getInvoice() {
+		return invoice;
+	}
+
+	public void setInvoice(Invoice invoice) {
+		this.invoice = invoice;
+	}
+
+	
+	
+	
+}
